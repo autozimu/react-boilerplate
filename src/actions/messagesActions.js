6ff4d19567199d5cda64544e02ce22d72e6cfd89
Jeff Li <junfengl@ca.ibm.com>
2017-04-05 14:47:53 -0400

    More explicit var name.

--- src/actions/messagesActions.js
+++ src/actions/messagesActions.js
@@ -1,7 +1,7 @@
 export function request() {
 	return dispatch => {
 		fetch("http://arch:8080/messages")
-			.then(res => res.json())
+			.then(response => response.json())
 			.then(messages => dispatch(set(messages)))
 			.catch(err => console.error(err));
 	};
