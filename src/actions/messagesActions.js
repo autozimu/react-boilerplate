bcbf9869c9f8ab571d0dc65b998bd3f015c38071
Jeff Li <junfengl@ca.ibm.com>
2017-04-05 14:54:51 -0400

    Single quotes in js.
    Conversely, double quotes in html.

--- src/actions/messagesActions.js
+++ src/actions/messagesActions.js
@@ -1,6 +1,6 @@
 export function request() {
 	return dispatch => {
-		fetch("http://arch:8080/messages")
+		fetch('http://arch:8080/messages')
 			.then(response => response.json())
 			.then(messages => dispatch(set(messages)))
 			.catch(err => console.error(err));
