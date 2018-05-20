cb355950e0cb70907c2e08bb300bd1cb37c2f98a
Junfeng Li <autozimu@gmail.com>
2018-05-20 14:31:43 -0700

    Upgrade all dependencies.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import 'semantic-ui-css/semantic.min.css';
+
 import { App } from './components/App';
 import { store } from './store';
 
